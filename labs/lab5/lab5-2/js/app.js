30365f0893fbe6f6abde095b0775343276df8604
k-dicken <78804757+k-dicken@users.noreply.github.com>
2021-09-29 11:29:09 -0400

    lab5

--- labs/lab5/lab5-2/js/app.js
+++ labs/lab5/lab5-2/js/app.js
@@ -1,26 +1,27 @@
-// function setup() {
-
     // loop 100 times
     for (i = 0; i < 100; i++) {
         
-        // set colors 255 * random
-        let color1 = 255 * Math.random();
-        let color2 = 255 * Math.random();
-        let color3 = 255 * Math.random();
-    
+        // set colors 255 * random with 1 for each r g b for full randomness
+        let colorR = 255 * Math.random();
+        let colorG = 255 * Math.random();
+        let colorB = 255 * Math.random();
+        
+        // create the div
         let squareDiv = document.createElement("div");
-        squareDiv.style.backgroundColor = "rgb(" + color1 + ", " + color2 + ", " + color3 + ")";
+
+        // set color to each random color
+        squareDiv.style.backgroundColor = "rgb(" + colorR + ", " + colorG + ", " + colorB + ")";
         console.log(squareDiv.style.backgroundColor);
+
+        // set width height & float
         squareDiv.style.width = "20px";
         squareDiv.style.height = "20px";
         squareDiv.style.float = "left";
-    
+
+        // place onto page
         document.body.appendChild(squareDiv);
 
     }
-
-
-// }
 
 
 
--- labs/lab5/lab5-3/js/app.js
+++ labs/lab5/lab5-3/js/app.js
@@ -1,17 +1,26 @@
+// function for changin div to blue
 function blue() {
     squareDiv.style.backgroundColor = "rgb(0, 0, 255)";
 }
 
+// function for changing div to black
 function black() {
     squareDiv.style.backgroundColor = "rgb(0, 0, 0)";
 }
 
+// creates
 let squareDiv = document.createElement("div");
-        squareDiv.style.width = "100px";
-        squareDiv.style.height = "100px";
-        squareDiv.style.backgroundColor = "rgb(0, 0, 255)"
-        squareDiv.setAttribute ("onmouseover", "black()")
-        squareDiv.setAttribute ("onmouseout", "blue()")
-        // squareDiv.style.float = "left";
+    // set width and height
+    squareDiv.style.width = "100px";
+    squareDiv.style.height = "100px";
 
-        document.body.appendChild(squareDiv);
+    // set staring bg color so its not blank
+    squareDiv.style.backgroundColor = "rgb(0, 0, 255)"
+
+    // use on mouse over to change to black using function
+    squareDiv.setAttribute ("onmouseover", "black()")
+    // use on mouse out to change to blue using function
+    squareDiv.setAttribute ("onmouseout", "blue()")
+
+    // place div on page
+    document.body.appendChild(squareDiv);
